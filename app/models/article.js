53ad2b12885ed7bc5a0af638d2751e9166848cd2
chris martinez and evan stewart <chris+evan@>
2016-10-25 11:35:15 -0700

    add routing for individual

--- app/models/article.js
+++ app/models/article.js
@@ -1,6 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  model(params){
+    return this.store.findRecord('article', params.article_id);
+  },
   title: DS.attr(),
   author: DS.attr(),
   image: DS.attr(),
